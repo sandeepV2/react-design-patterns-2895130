3321d96d4c25b2174850b958907c329c9cee9f55
Sandeep Belagavi <sbelagavi@cloudera.com>
2023-07-07 15:16:08 +0530

    Adding splitScreen for side nav bar

---
+++ src/splitScreen.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const splitScreen = ({
+    left: Left,
+    right: Right,
+}) => {
+  return (
+    <div>
+     <div>Left</div>
+     <div>Right</div>
+    </div>
+  )
+}
+
+export default splitScreen;
